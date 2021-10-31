b1bedc55b5170495568c873914db425b7b203fd0
Sharad Sharma <44286121+sharad-sharma@users.noreply.github.com>
2021-10-31 15:22:02 +0530

    Adding c.cpp
    Adding solution to problem C

---
+++ c.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(a) begin(a), end(a)
+#define deb(x) cout << #x << " = " << x << endl
+#define debu(x, y) cout << "( " << #x << ", " << #y << " ) = ( " << x << ", " << y << " )" << endl
+#define logarr(arr) for(int z=(0); z<((int)arr.size()); z++) cout << (arr[z]) << " "; cout << endl
+
+int32_t main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	string s;
+	int k;
+	cin >> s >> k;
+
+	sort(all(s));
+
+	int v = 0;
+	do {
+		v++;
+		if(v == k) break;
+	} while(next_permutation(all(s)));
+
+	cout << s << "\n";
+
+	return 0;
+}
